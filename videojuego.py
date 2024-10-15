1baa6d962981655527b6b7340c0ff14fd8459897
RampantBag43488 <inakimancera@gmail.com>
2024-10-14 21:26:11 -0600

    Update videojuego comentarios.py

--- videojuego.py
+++ videojuego.py
@@ -1,43 +1,52 @@
-#primero se declara una lista como vjp (videojuego perfecto) para guardar las 
-#caracteristicas deseadas en el juego por el usuario.
-vdp=[]
+"""
+Proyecto TC1028
+Cuestionario para el videojuego perfecto.
+El cuestionario realiza una serie de preguntas para definir las caracteristicas
+en un juego que más prefiere el usuario y compara estas caracteristicas con las
+que tienen distintos juegos para hallar el que tenga más coincidencia y por
+consiguiente sea el juego indicado para el usuario.
+Le da al usuario el o los juegos que más coincidencias tuvieran con sus 
+preferencias.
+"""
 
-#luego se crea un diccionario de listas de cada juego en la que se guardan las caracteristicas que
-#corresponden al juego en la llave.
-videogames_dictionary = {"minecraft" : ['P', 'xbox', 'pc', 'play', 'coop', 'aventura', 'low-poly', 'pixelado', \
-'supervivencia', 'game pass'], "minecraft_dungeons" : ['P', 'xbox', 'pc', 'play', 'coop', 'aventura', 'pixelado', \
-'dungeons', 'game pass'], "cod_warzone" : ['G', 'xbox', 'pc', 'play','multi', 'accion', 'realista', 'ninguno', \
-'guerra', 'battle royal']}
+"""
+=================Diccionario de videojuegos con caracteristicas=================
+"""
+videogames_dictionary = {"minecraft" : ['P', 'xbox', 'pc', 'play', 'coop', 
+'aventura', 'low-poly', 'pixelado', 'supervivencia', 'game pass'], 
+"minecraft_dungeons" : ['P', 'xbox', 'pc', 'play', 'coop', 'aventura', 
+'pixelado', 'dungeons', 'game pass'], "cod_warzone" : ['G', 'xbox', 'pc', 
+'play','multi', 'accion', 'realista', 'ninguno', 'guerra', 'battle royal']}
 
 
-
-
-
-#se da una introduccion al usuario para que sepa que hacer y para que sob las preguntas
-#que tendra que responder.
 print("Hola usuario, espero te encuentres bien. \nA continuacion se te pediran \
 ciertas caracteristicas a tu gusto que definiran el videojuego mas apropiado \
 para ti en tu proximo juego")
 
-#se hace una funcion con el proceso de pedir las caracteristicas al usuario y guardarlas 
-#en lista para que si el usuario al acabar su primer inteno quiere probar con distintas
-#caracteriasticas le sea posible.
+"""
+==============================Funcion de preguntas==============================
+"""
 def lista_usu():
+    """
+    (uso de listas, funciones)
+    Recopila las caracteristicas que el usuario quiere en un juego en una lista.
+    Crea una lista con las caracteristicas que el usuario desee en su juego en
+    base a siete preguntas.
+    Devuelve: la lista con las caracteristicas que el usuario decide.
+    """
     vjp=[]
-    #cada pregunta es sobre una caracteristica distinta que lo que responda el usuario
-    #se va guardando como un nuevo dato en la lista vjp gracias a la funcion .append
-    #que agrega el input recibido como un valor nuevo hasta el final de la lista vjp.
-    vjp.append(input("\nPrimero.\n\n¿Prefieres un juego gratuito o de paga? (pon G o P)\n"))
+    vjp.append(input("\nPrimero.\n\n¿Prefieres un juego gratuito o de paga? \
+(pon G o P)\n"))
 
     vjp.append(input("\n\nAhora bien dime, ¿Que plataforma de juego prefieres? \
 (xbox, pc o play)\n"))
 
-    vjp.append(input("\n\nEmpezamos con lo mas importante, ¿Prefieres jugar en solitario, \
-un cooperativo o un multijugador? (solo, coop, multi)\n"))
+    vjp.append(input("\n\nEmpezamos con lo mas importante, ¿Prefieres jugar en \
+solitario, un cooperativo o un multijugador? (solo, coop, multi)\n"))
 
     vjp.append(input("\n\n¿Y que hay del genero que te interesa mas?\n\
-(Los generos son: plataformas, puzles, simulacion, RPG, accion, aventura, estrategia, \
-deportes, terror, cifi, fantasia y carreras)\n"))
+(Los generos son: plataformas, puzles, simulacion, RPG, accion, aventura, \
+estrategia, deportes, terror, cifi, fantasia y carreras)\n"))
 
     vjp.append(input("\n\nTambien es importante la apariencia o estilo\
 de un juego, ¿Cual te gusta mas?\n(pixelado, realista, retro, \
@@ -45,45 +54,71 @@ surrealista, dibujo a mano, multimedia, dibujo animado, noir o \
 low-poly)\n"))
 
 
-    vjp.append(input("\n\n¿Quieres que el juego se encuentre en el game pass, play plus \
-o ninguno?\n"))
+    vjp.append(input("\n\n¿Quieres que el juego se encuentre en el game pass, \
+play plus o ninguno?\n"))
                     
-    vjp.append(input("\n\nPor ultimo, pero no menos importante dime que tematica principal \
-prefieres.\n(supervivencia, guerra, carros, aviones, espacio, superheroes, zombies, medieval,\
-rutas multiples, granjas, robots, piratas, peleas, playground, escape, dungeons, \
-football, basquet, golf, ciclismo, souls like, lego, battle royal, mundo abierto)\n"))
+    vjp.append(input("\n\nPor ultimo, pero no menos importante dime que \
+tematica principal prefieres.\n(supervivencia, guerra, carros, aviones, \
+espacio, superheroes, zombies, medieval, rutas multiples, granjas, \
+robots, piratas, peleas, playground, escape, dungeons, football, basquet, \
+golf, ciclismo, souls like, lego, battle royal, mundo abierto)\n"))
     return vjp
 
-
-#la funcion agarra las dos listas elegidas, usa la funcion set para quitar valores repetidos
-#en ambas y usa la funcion & para sacar la interseccion entre ambas listas para tener como
-#resultado una lista de valores en comun entre ambas listas. Luego la funcion len cuenta la
-#cantidad de valores en la lista de valores encontrados en ambas listas y devuelve ese conteo.
+"""
+=======================Funciones principales del programa=======================
+"""
 def num_valores_comun(lista1, lista2):
+    """
+    (uso de funciones, lista, diccionario)
+    Recibe: la lista de caracteristicas que hizo el usuario y los valores de 
+    uno de los juegos en el diccionario.
+    Se comparan los valores de la lista hecha del usuarion y los de un juego de 
+    el diccionario.
+    Devuelve: la cantidad de valores iguales entre la lista del usuario y los 
+    valores del juego en el diccionario.
+    """
     return len(set(lista1) & set(lista2))
 
-#Se guardan en un diccionario como llaves los nombres de los juegos y como valores la cantidad
-#de coincidencias que hay con la lista de caracteristicas del usuario
 def valores_comparacion():
+    """
+    (uso de funciones, lista,diccionarios, ciclos, condicionales)
+    Recibe: lista creada por el usuario de caracteristicas deseadas y 
+    diccionario de videojuegos con sus caracteristicas.
+    Usa la funcion num_valores_comun(lista1, lista2) y pasa por cada juego 
+    en el diccionario para que se haga la comparación con todos y el resultado
+    de la comparacion se guarda en un diccionario nuevo con el nombre del juego
+    como llave y la cantidad de coincidencias como valor.
+    Devuelve: diccionario con el nombre de los videojuegos como llave y de valor
+    la cantidad de coincidencias de caracteristicas de ese juego con la lista 
+    hecha por el usuario.
+    """
     vp=lista_usu()
     compa_dictionary = {}
     for i in videogames_dictionary:
         compa_dictionary.update({i : num_valores_comun(vp,videogames_dictionary[i])})
     return compa_dictionary
 
-#se compara que videojuego tiene mas coincidencias en caracteristicas con las pedidas por
-#el usuario y te da el resultado.
+
 def comparar_resultados(a):
+    """
+    (uso de funciones, ciclos, ciclos anidados, diccionarios, lista, 
+    condicionales)
+    Recibe: El diccionario de cantidad de coincidencias con la lista del usuario
+    respecto a cada juego resultante de la funcion valores_comparacion().
+    Guarda en una variable el nombre de el juego con más coincidencias y 
+    luego por si hay más juegos con la misma cantidad de coincidencias 
+    se guardan todos los juegos con mayor numero de coincidencias en una lista.
+    Devuelve: Una lista con el o los nombres de los juegos con mayor cantidad
+    de coincidencias que la lista creada por el usuario.
+    """
     c_dicc = a
     list_res = []
     temp = "minecraft"
     for x in c_dicc:
         if c_dicc[x] >= c_dicc[temp]:
             v_definitivo = x
-            print(v_definitivo)
         elif c_dicc[temp] < c_dicc[x]:
             v_definitivo = temp
-            print(v_definitivo)
         temp = x
 
     list_res.append(v_definitivo)
@@ -93,17 +128,30 @@ def comparar_resultados(a):
     
     return list_res
 
-#Funcion para definir que respuesta sele dara al usuario en base de si es uno o 
-#más la cantidad de juegos que se le recomendaran
 def respuesta(lista):
+    """
+    (Uso de funciones, lista, condicionales)
+    Recibe: La lista con el o los nombres de los juegos con mayor cantidad
+    de coincidencias que la lista creada por el usuario, resultante de la 
+    funcion comparar_resultados(a).
+    Devuelve: La respuesta sobre que juego o juegos son los más indicados 
+    para el usuario.
+    """
     if len(lista) == 1:
         return "El videojuego perfecto para ti es: ", lista
     else:
         return "Los videojuegos perfectos para ti son: ", lista
 
-#funcion para volver a preguntar caracteristicas al usuario y hacer la comparacion si el
-#usuario quiere repetir
+
 def repetir():
+    """
+    (uso de funciones, ciclos, condicionales)
+    Recibe: Si el usuario quiere repetir o no la selección de caracteristicas y
+    así obtener un resultado distinto dependioendo de la selección.
+    Devuelve: La funcion lista_usu() para volver a hacer selección de 
+    caracteristicas en caso de responde si y en caso de responder no, se le 
+    desea un buen dia al usuario y acaba el programa.
+    """
     answer=int(input("pon un 1 si quieres poner distintas caracteristicas y obtener \
 otro juego y 0 si no "))
 
@@ -116,10 +164,6 @@ otro juego y 0 si no "))
     else:
         return "ten un buen dia usuario!"
 
-#Se imprime al usuario el codigo para recopilar su lista de caracteristicas preferidas y 
-#cual es el juego o juegos que más concuerdan
+#Se muestra el cuestionario de caracteristicas con su resultado y repetir().
 print(respuesta(comparar_resultados(valores_comparacion())))
-
-#Se imprime la funcion de repetir que le da la opcion al usuario de volver a dar caracteriticas
-#y obtener un juego distinto como resultado si las respuestas son distintas
 print(repetir())
